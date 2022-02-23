d1c4adb886efdd02ca1197103cc325d3833b30ce
mirarukun <miraru.395091@gmail.com>
2022-02-24 00:28:28 +0900

    バリデーション記述

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -8,4 +8,17 @@ belongs_to :genre
 belongs_to :time_required
 belongs_to :situation
 belongs_to :direction
+
+with_options presence: true do
+  validates :name
+  validates :score, inclusion: { in: 0..10 }, format: { with: /\A[0-9]+\z/ }
+  validates :text
+end
+with_options numericality: { other_than: 0, message: "can't be blank" } do
+  validates :genre_id
+  validates :time_required_id
+  validates :situation_id
+  validates :direction_id
+end
+validates :score, numericality: true
 end
