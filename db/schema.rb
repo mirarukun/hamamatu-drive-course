315e323cb7129cb64f4062ef5218d73370b949c1
mirarukun <miraru.395091@gmail.com>
2022-02-23 22:58:44 +0900

    spotモデルの作成とマイグレーションファイルへテーブルの作成を行なった

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_090512) do
+ActiveRecord::Schema.define(version: 2022_02_23_133538) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "score", null: false
+    t.text "text", null: false
+    t.integer "genre_id", null: false
+    t.integer "time_required_id", null: false
+    t.integer "situation_id", null: false
+    t.integer "direction_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_02_23_090512) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spots", "users"
 end
