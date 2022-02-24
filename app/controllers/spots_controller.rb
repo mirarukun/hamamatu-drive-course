0b4b70c53aadac539fa8f3c1329fb8270d67f973
mirarukun <miraru.395091@gmail.com>
2022-02-24 12:52:28 +0900

    imageカラム忘れないよう、先にストロングパラメーター設定した

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,11 @@
 class SpotsController < ApplicationController
   def index
   end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :score, :text, :genre_id, :time_required_id,
+      :situation_id, :direction_id, :image).merge(user_id: current_user.id)
+  end
 end
