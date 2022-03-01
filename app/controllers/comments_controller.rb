f19e2e18c425298f989932315a00f6436b04c8ee
mirarukun <miraru.395091@gmail.com>
2022-03-01 21:57:52 +0900

    createアクションをcommentsコントローラーに定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @Comment = Comment.new(spot_params)
+    if @comment.save
+      redirect_to spot_path(@comment.spot)
+    else
+      @spot = @comment.spot
+      @comments = @spot.comments
+      render "spots/show"
+    end
+  end
+
+  private
+  def spot_params
+    params.require(:comment).permit(:score, :text).merge(user_id: current_user.id, spot_id: params[:spot_id])
+  end
+end
