858b47cb2b3ac15140e73ea51c8800b0b644fcaf
mirarukun <miraru.395091@gmail.com>
2022-03-01 20:15:53 +0900

    バリデーションを設定した

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user  
   belongs_to :spot
+
+  with_options presence: true do
+    validates :score, inclusion: { in: 0..10 }, format: { with: /\A[0-9]+\z/ } , numericality: true
+    validates :text
+  end
 end
