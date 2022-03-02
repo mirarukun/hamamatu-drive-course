b1352ef41f68cd4cceb2b4d87cba478bdaab99d9
mirarukun <miraru.395091@gmail.com>
2022-03-02 21:11:59 +0900

    spotモデルの単体テストコードを作成した

--- spec/factories/spots.rb
+++ spec/factories/spots.rb
@@ -3,10 +3,11 @@ FactoryBot.define do
     name { '細江公園' }
     score { '5' }
     text { Faker::Lorem.sentence }
-    genre_id { Genre.all.sample }
-    time_required_id { TimeRequired.all.sample }
-    situation_id { Situation.all.sample }
-    direction { Direction.all.sample }
+    genre_id { Genre.all.sample.id }
+    time_required_id { TimeRequired.all.sample.id }
+    situation_id { Situation.all.sample.id }
+    direction_id { Direction.all.sample.id }
+    association :user 
 
     after(:build) do |spot|
       spot.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
